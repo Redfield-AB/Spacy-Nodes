e9853dca6287df98f0e7373312dc155c5d11a153
Alexander Bondaletov <alexander.bondaletov@redfield.se>
2022-06-12 14:16:50 +0300

    Extend NER tag set

--- plugin/src/se/redfield/textprocessing/data/tag/SpacyNerTagBuilder.java
+++ plugin/src/se/redfield/textprocessing/data/tag/SpacyNerTagBuilder.java
@@ -3,15 +3,7 @@
 */
 package se.redfield.textprocessing.data.tag;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.knime.ext.textprocessing.data.Tag;
-import org.knime.ext.textprocessing.data.TagBuilder;
-
-public class SpacyNerTagBuilder implements TagBuilder {
+public class SpacyNerTagBuilder extends ResourceFileTagBuilder {
 
 	private static final SpacyNerTagBuilder instance = new SpacyNerTagBuilder();
 
@@ -23,31 +15,7 @@ public class SpacyNerTagBuilder implements TagBuilder {
 	}
 
 	public SpacyNerTagBuilder() {
-		// default constructor
-	}
-
-	@Override
-	public Tag buildTag(String value) {
-		SpacyNerTag tag = SpacyNerTag.fromString(value);
-		if (tag == SpacyNerTag.UNKNOWN) {
-			return null;
-		}
-		return tag.getTag();
-	}
-
-	@Override
-	public List<String> asStringList() {
-		return Arrays.stream(SpacyNerTag.values()).map(t -> t.getTag().getTagValue()).collect(Collectors.toList());
-	}
-
-	@Override
-	public Set<Tag> getTags() {
-		return Arrays.stream(SpacyNerTag.values()).map(SpacyNerTag::getTag).collect(Collectors.toSet());
-	}
-
-	@Override
-	public String getType() {
-		return SpacyNerTag.TAG_TYPE;
+		super("SPACY_NE", "config/ner_tags.txt");
 	}
 
 }
