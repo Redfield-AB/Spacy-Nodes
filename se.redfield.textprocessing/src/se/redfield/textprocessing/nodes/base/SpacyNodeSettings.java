bec1036ceac62e4a93412a5c6fddb5c3c9c804bc
Adrian Nembach <adrian.nembach@knime.com>
2022-07-25 13:19:47 +0200

    Use PythonPreferences instead of DLPythonPreferences
    This is only temporary until the Spacy extension has its own preference
    page.

--- se.redfield.textprocessing/src/se/redfield/textprocessing/nodes/base/SpacyNodeSettings.java
+++ se.redfield.textprocessing/src/se/redfield/textprocessing/nodes/base/SpacyNodeSettings.java
@@ -3,14 +3,15 @@
 */
 package se.redfield.textprocessing.nodes.base;
 
+import org.knime.conda.prefs.CondaPreferences;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
-import org.knime.dl.python.prefs.DLPythonPreferences;
 import org.knime.python2.PythonVersion;
 import org.knime.python2.config.PythonCommandConfig;
+import org.knime.python2.prefs.PythonPreferences;
 
 /**
  * The node settings for the different SpaCy nodes.
@@ -46,7 +47,7 @@ public class SpacyNodeSettings {
 		replaceColumn = new SettingsModelBoolean(KEY_REPLACE_COLUMN, defReplaceColumn);
 		appendedColumnName = new SettingsModelString(KEY_APPENDED_COLUMN_NAME, defAppendedColumnName);
 		pythonCommand = new PythonCommandConfig(KEY_PYTHON_COMMAND, PythonVersion.PYTHON3,
-				DLPythonPreferences::getCondaInstallationPath, DLPythonPreferences::getPythonTF2CommandPreference);
+				CondaPreferences::getCondaInstallationDirectory, PythonPreferences::getPython3CommandPreference);
 
 		appendedColumnName.setEnabled(!defReplaceColumn);
 		replaceColumn.addChangeListener(e -> appendedColumnName.setEnabled(!replaceColumn.getBooleanValue()));
